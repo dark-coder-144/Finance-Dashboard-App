6bbbf78789914f19e0b5952899fdbfb029982908
K SHRAVAN <86780079+dark-coder-144@users.noreply.github.com>
2023-10-09 15:10:25 +0530

    Routing and Navigation Done

--- finance-dashboard-app/src/App.tsx
+++ finance-dashboard-app/src/App.tsx
@@ -1,4 +1,25 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import {useMemo} from "react"
+import { themeSettings } from "./theme";
+import { Box, CssBaseline } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "@/components/navbar";
+
 function App() {
-  return <div className="app"></div>
+  const theme = useMemo(()=> createTheme(themeSettings), [])
+  return <div className="app">
+    <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<div>dashboard page</div>}/>
+          <Route path="/predictions" element={<div>prediction page</div>}/>
+        </Routes>
+      </Box>
+    </ThemeProvider>
+    </BrowserRouter>
+  </div>
 }
 export default App;
